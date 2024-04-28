f0aeeb4bb62f2608652d9f46de439c46a2abb0da
batuhan <lelbatuhan@gmail.com>
2024-04-28 23:09:43 +0200

    initial commit

---
+++ src/main/java/com/bookwhile/BookWhileApplication.java
@@ -0,0 +1,13 @@
+package com.bookwhile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWhileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookWhileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookwhile/BookWhileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookwhile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWhileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
