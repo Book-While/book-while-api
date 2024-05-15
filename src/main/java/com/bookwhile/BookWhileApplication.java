0ef95f59a890e2a66520fda74582c1bd7f6d8622
Sercan <sercanharputlu@gmail.com>
2024-05-15 15:16:14 +0300

    feat: add custom checkstyle.xml and Javadoc to BookWhileApplication.java BW-5

--- src/main/java/com/bookwhile/BookWhileApplication.java
+++ src/main/java/com/bookwhile/BookWhileApplication.java
@@ -3,9 +3,22 @@ package com.bookwhile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the BookWhile application.
+ * <p>
+ * This class initializes and runs the Spring Boot application.
+ * </p>
+ *
+ * @see org.springframework.boot.SpringApplication
+ */
 @SpringBootApplication
 public class BookWhileApplication {
 
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args Command line arguments (if any).
+     */
     public static void main(String[] args) {
         SpringApplication.run(BookWhileApplication.class, args);
     }
